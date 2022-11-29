f453fb31fc99351b3695938b25b469c47adb07a9
Alejandro Paredes <alejocangrejo10@gmail.com>
2022-11-29 00:08:19 -0400

    First upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAppJokesApp.Models;
+
+namespace WebAppJokesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebAppJokesApp.Models.Joke> Joke { get; set; }
+    }
+}
